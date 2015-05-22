90c99154d685edf2697592d1b9f2f81c0fe9e6a5
saki18 <takaha18@msu.edu>
2015-05-22 00:31:38 -0400

    Hello World
    Hello World Program

---
+++ Archive/Hello_World.java
@@ -0,0 +1,23 @@
+/**     ___  ___       ___   _____       ___   _   _    _  
+*      /   |/   |     /   | /  ___/     /   | | | / /  | | 
+*     / /|   /| |    / /| | | |___     / /| | | |/ /   | | 
+*    / / |__/ | |   / / | | \___  \   / / | | | |\ \   | | 
+*   / /       | |  / /  | |  ___| |  / /  | | | | \ \  | | 
+*  /_/        |_| /_/   |_| /_____/ /_/   |_| |_|  \_\ |_| 
+*
+*/ package hello_world;
+/**
+ *
+ * @author Masaki Takahashi
+ * CITP 190
+ * Source: Hello_World.Java
+ * Project: Hand out project 1
+ * Description: Hello World, generic programming output. 
+ */
+public class Hello_World {
+
+    public static void main(String[] args) {
+        System.out.println("Hello World");
+    }
+    
+}
